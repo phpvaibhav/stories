e8260dae980c2a9ae1ca6b6059c773904e1cd646
user <phpvaibhav@gmail.com>
2019-09-18 17:36:59 +0530

    correction page

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -13,5 +13,21 @@ class Home extends Common_Front_Controller {
     public function index() { 
         $data['page_title'] = "Home";
     	$this->load->front_render('home', $data, '');
+    } 
+    public function contactus() { 
+        $data['page_title'] = "Contact us";
+    	$this->load->front_render('contact', $data, '');
+    } 
+    public function aboutus() { 
+        $data['page_title'] = "About us";
+    	$this->load->front_render('aboutus', $data, '');
+    } 
+    public function termConditions() { 
+        $data['page_title'] = "Term & Conditions";
+    	$this->load->front_render('termConditions', $data, '');
+    }
+    public function privacyPolicy() { 
+        $data['page_title'] = "Privacy Policy";
+    	$this->load->front_render('privacyPolicy', $data, '');
     }
 }//end class
